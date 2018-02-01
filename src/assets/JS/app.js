25bdaa6c7bf4a800b18b836014be99e3ccf15b23
kraken1970 <kraken@hi.ru>
2018-02-01 20:14:42 +0300

    1-st commit01021018

---
+++ src/assets/JS/app.js
@@ -0,0 +1,14 @@
+$('.carousel').carousel({
+  interval: 2000,
+  // keyboard: false,
+  // отключить управление клавиатурой
+  // wrap:false
+  // запрет автопрокрутки после окончантя цикла
+});
+
+$('#exampleModal').modal({
+  // keyboard: false,
+  //нет закрытия по esc
+  // backdrop: "static"
+  // не закроется по нажатию на пустую область
+})
